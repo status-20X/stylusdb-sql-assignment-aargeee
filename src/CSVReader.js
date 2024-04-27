339786b3a6350b86ddcb85689b9c58c9b6d3b972
aargeee <211220042@nitdelhi.ac.in>
2024-04-28 01:40:19 +0530

    Step 2: Complete

---
+++ src/CSVReader.js
@@ -0,0 +1,13 @@
+import fs from "fs";
+import csv from "csv-parser";
+
+export default readCSV = (filePath) => {
+  const results = [];
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(filePath)
+      .pipe(csv())
+      .on("data", (data) => results.push(data))
+      .on("end", () => resolve(results))
+      .on("error", (err) => reject(err));
+  });
+};
--- test/index.test.js
+++ test/index.test.js
@@ -1,3 +1,12 @@
-test("Hello Jest", () => {
-  expect("aargeee").toBe("aargeee");
+import readCSV from "../src/CSVReader";
+
+test("Read CSV", async () => {
+  const data = await readCSV("./aargeee.csv");
+  expect(data.length).toBeGreaterThanOrEqual(3);
+  const want = [
+    { id: "1", name: "akku", age: "20" },
+    { id: "2", name: "aargeee", age: "21" },
+    { id: "3", name: "everyone", age: "28" },
+  ];
+  expect(data).toStrictEqual(want);
 });
