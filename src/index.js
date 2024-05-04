eea08ba4da80302475619dacb9296325868514f6
aargeee <211220042@nitdelhi.ac.in>
2024-05-04 16:21:45 +0530

    step 5 done

--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ const parseQuery = require("./queryParser")
 
 const executeSELECTQuery = async (query) => {
 
-    const {fields, table} = parseQuery(query);
+    const {fields, table, whereClause} = parseQuery(query);
     const data = await readCSV(`${table}.csv`);
 
-    return data.map(row => {
+    const filteredData = whereClause ? 
+        data.filter(row => {
+            const [field, value] = whereClause.split("=").map(s => s.trim())
+            return row[field] == value
+        }) : data;
+
+    return filteredData.map(row => {
         const filteredRow = {};
         fields.forEach(field => {
             if (!(field in row)) throw new Error("Field DNE");
--- src/queryParser.js
+++ src/queryParser.js
@@ -1,12 +1,13 @@
 const parseQuery = (query) => {
-    const SELECTQueryRegex = /SELECT (.+) FROM (.+)/i;
+    const SELECTQueryRegex = /SELECT (.+?) FROM (.+?)(?: WHERE (.*))?$/i;;
     const match = query.match(SELECTQueryRegex);
 
     if (match) {
-        const [, columns, table] = match;
+        const [, columns, table, whereClause] = match;
         return {
             fields: columns.split(",").map(col => col.trim()),
-            table: table.trim()
+            table: table.trim(),
+            whereClause: whereClause ? whereClause.trim() : null
         }
     } else {
         throw new Error("Invalid Query Format")
